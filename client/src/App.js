6f78aa34ec10417bc580a70642093dc8badd992d
zhoupeng1998 <rdfzzhoupeng@gmail.com>
2019-11-13 18:14:24 -0800

    chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+/*import './App.css';*/
 import axios from 'axios';
+import {Line as LineChart} from 'react-chartjs-2';
 //const axios = require('axios');
 
 class App extends Component {
     state = {
-        data: '',
+        time: [],
+        temperature: [],
+        humidity: []
     }
 
     constructor () {
@@ -18,10 +21,9 @@ class App extends Component {
         this.setState({data: '2'})
         var _this = this;
         axios.get('http://127.0.0.1:8080/record').then((res) => {
-            _this.setState({data: res.data.time[0]});
-            _this.setState({time: res.data.time});
-            _this.setState({temperature: res.data.temperature});
-            _this.setState({humidity: res.data.humidity});
+            _this.setState({time: res.data.time.reverse()});
+            _this.setState({temperature: res.data.temperature.reverse()});
+            _this.setState({humidity: res.data.humidity.reverse()});
 
         }).catch(err => {
             _this.setState({data: 'ERROR'});
@@ -29,17 +31,46 @@ class App extends Component {
     }
 
     render () {
+        var chart = {
+            labels: this.state.time,
+            datasets: [
+                /*{
+                    label: 'My First dataset',
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                }*/
+                //,
+                {label: "Temperature", data: this.state.temperature, borderColor: 'rgba(75,192,192,1)', fill:false},
+                {label: "Humidity", data: this.state.humidity, borderColor: 'rgba(255, 0, 0, 1)', fill:false}
+                //*/
+            ]
+        }
+
+        const options = {
+            title: {
+                display: true,
+                fill: false
+            }
+        }
         return (
             <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    {this.state.data}
-                </p>
-              </header>
+                <LineChart data={chart} options={options} width='1000' height='500' />
             </div>
           );
     }
